97eac5b0aea79dbe370a8c7b8d0efe55862c0f55
Jasmin Dzanic <jasmin@dzanic.se>
2025-07-09 16:07:38 +0200

    Remove Year from Movie and add migration

--- MovieApi/Models/Movie.cs
+++ MovieApi/Models/Movie.cs
@@ -7,7 +7,6 @@ namespace MovieApi.Models
         public int Id { get; set; }
         public string Title { get; set; } = null!;
         public int ReleaseYear { get; set; }
-        public int Year { get; set; }
         public string Genre { get; set; } = null!;
         public int Duration { get; set; }
 
